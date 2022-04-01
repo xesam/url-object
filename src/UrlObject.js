f4deb32d91961cbef09e427e1f5ea85261e7bb94
xesam <xesamguo@gmail.com>
2022-04-01 10:03:53 +0800

    init

--- src/UrlObject.js
+++ src/UrlObject.js
@@ -1,4 +1,58 @@
+const _URL = typeof URL === 'function' ? URL : null;
 
+class UrlObject {
+    constructor(urlString, base = '') {
+        this._url = new _URL(urlString, base);
+    }
 
+    protocol(value) {
+        return this._url.protocol;
+    }
 
-module.exports = null;
+    username(value) {
+        return this._url.username;
+    }
+
+    password(value) {
+        return this._url.password;
+    }
+
+    host(value) {
+        return this._url.host;
+    }
+
+    hostname(value) {
+        return this._url.hostname;
+    }
+
+    port(value) {
+        return this._url.port;
+    }
+
+    pathname(value) {
+        return this._url.pathname;
+    }
+
+    search(value) {
+        return this._url.search;
+    }
+
+    searchParams(value) {
+        return this._url.searchParams;
+    }
+
+    hash(value) {
+        return this._url.hash;
+    }
+
+    origin(value) {
+        return this._url.origin;
+    }
+
+    href(value) {
+        return this._url.href;
+    }
+
+}
+
+module.exports = UrlObject;
